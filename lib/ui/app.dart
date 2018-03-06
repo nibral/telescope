bf4ea963a293b4950f734961a727e560f4790eac
nibral <nibral@users.noreply.github.com>
2018-03-06 15:26:00 +0900

    change to grid list

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:telescope/model/character_list_item.dart';
 import 'package:telescope/repository/character_repository.dart';
 import 'package:telescope/repository/repository_factory.dart';
 import 'package:telescope/ui/character_detail_page.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,6 +23,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<CharacterListItem> _characterList = [];
   Map<int, Widget> _icons = new Map();
+
+  Widget _iconPlaceholder = const SizedBox(
+    height: 128.0,
+    width: 128.0,
+  );
 
   @override
   void initState() {
@@ -42,22 +49,50 @@ class _MyHomePageState extends State<MyHomePage> {
               }),
         ],
       ),
-      body: new ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          var character = _characterList[index];
-          _loadIcon(character.id);
-          return new ListTile(
-            leading: _icons[character.id],
-            title: new Text(character.name),
-            subtitle: new Text(character.name_kana),
-            onTap: () {
-              Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (_) => new CharacterDetailPage(character.id),
-                  ));
-            },
-          );
-        },
-        itemCount: _characterList.length,
+      body: new Center(
+        child: new GridView.builder(
+          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 128.0,
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+          ),
+          padding: const EdgeInsets.all(4.0),
+          itemBuilder: (BuildContext context, int index) {
+            var character = _characterList[index];
+            _loadIcon(character.id);
+            return new InkWell(
+              onTap: () {
+                Navigator.of(context).push(new MaterialPageRoute(
+                      builder: (_) => new CharacterDetailPage(character.id),
+                    ));
+              },
+              child: new Stack(
+                children: <Widget>[
+                  _icons[character.id],
+                  new Positioned(
+                    child: new Container(
+                      child: new Text(
+                        character.name,
+                        style: new TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      decoration: new BoxDecoration(
+                        color: const Color.fromRGBO(0, 0, 0, 0.5),
+                      ),
+                      padding: const EdgeInsets.only(left: 4.0),
+                    ),
+                    width: 128.0,
+                    height: 32.0,
+                    left: 0.0,
+                    bottom: 0.0,
+                  ),
+                ],
+              ),
+            );
+          },
+          itemCount: _characterList.length,
+        ),
       ),
     );
   }
@@ -68,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
     repository.getList(refresh).then((r) {
       setState(() {
         r.values.forEach((c) {
-          _icons[c.id] = new Icon(Icons.account_circle);
+          _icons[c.id] = _iconPlaceholder;
         });
         _characterList = r.values.toList();
         _characterList.sort((a, b) {
@@ -83,7 +118,10 @@ class _MyHomePageState extends State<MyHomePage> {
         await new RepositoryFactory().getCharacterRepository();
     repository.find(id).then((c) {
       setState(() {
-        _icons[id] = new Image.network(c.icon_image_ref);
+        _icons[id] = new CachedNetworkImage(
+          imageUrl: c.icon_image_ref,
+          placeholder: _iconPlaceholder,
+        );
       });
     });
   }
