1be3e085ab510002fec5bd576646bb3d60ab1986
nibral <nibral@users.noreply.github.com>
2018-03-09 15:57:40 +0900

    improve design of detail page

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onTap: () {
                 Navigator.of(context).push(new MaterialPageRoute(
                       builder: (_) => new CharacterDetailPage(
-                          character.id, character.cardIdList),
+                          character.name, character.cardIdList),
                     ));
               },
               child: new Stack(
--- lib/ui/character_detail_page.dart
+++ lib/ui/character_detail_page.dart
@@ -10,10 +10,10 @@ import 'package:telescope/repository/repository_factory.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class CharacterDetailPage extends StatefulWidget {
-  final int id;
+  final String name;
   final List<int> cardIdList;
 
-  const CharacterDetailPage(this.id, this.cardIdList);
+  const CharacterDetailPage(this.name, this.cardIdList);
 
   @override
   State<StatefulWidget> createState() {
@@ -23,8 +23,6 @@ class CharacterDetailPage extends StatefulWidget {
 
 class _CharacterDetailPageState extends State<CharacterDetailPage> {
   bool isLoading = true;
-  String characterName = '';
-  Widget characterDetail;
   List<Widget> cardImages = [];
 
   @override
@@ -47,7 +45,12 @@ class _CharacterDetailPageState extends State<CharacterDetailPage> {
     return new Scaffold(
       appBar: isPortrait
           ? new AppBar(
-              title: new Text(characterName),
+              title: new Text(
+                widget.name,
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               centerTitle: true,
             )
           : null,
@@ -57,15 +60,12 @@ class _CharacterDetailPageState extends State<CharacterDetailPage> {
             )
           : new CustomScrollView(
               slivers: <Widget>[
-                new SliverToBoxAdapter(
-                  child: characterDetail,
-                ),
                 new SliverList(
                   delegate: new SliverChildListDelegate(cardImages),
                 ),
                 new SliverToBoxAdapter(
                   child: new Container(
-                    height: 32.0,
+                    height: 16.0,
                     color: Colors.transparent,
                   ),
                 ),
@@ -75,48 +75,47 @@ class _CharacterDetailPageState extends State<CharacterDetailPage> {
   }
 
   void _loadCharacterDetail() async {
-    CharacterRepository characterRepository =
-        await new RepositoryFactory().getCharacterRepository();
-
-    Character character = await characterRepository.find(widget.id);
-    characterName = character.name;
-    characterDetail = new Column(
-      children: <Widget>[
-        new CachedNetworkImage(imageUrl: character.iconImageUrl),
-        new Text(character.name),
-        new Text(character.nameKana),
-      ],
-    );
-
-    setState(() {
-      isLoading = false;
-      cardImages = [
-        new Center(
-          child: new CircularProgressIndicator(),
-        )
-      ];
-    });
-
     CardRepository cardRepository = new RepositoryFactory().getCardRepository();
     List<Widget> images = [];
 
     await Future.forEach(widget.cardIdList, (id) async {
       CharacterCard.Card card = await cardRepository.find(id);
       if (card.spreadImageUrl != null) {
-        images.add(new CardSpreadImage(card.name, card.spreadImageUrl));
+        images.add(new Container(
+          child: new CardSpreadImage(card.name, card.spreadImageUrl),
+          padding: const EdgeInsets.only(top: 16.0),
+        ));
       }
 
       if (card.evolutionCardId != 0) {
         CharacterCard.Card evoCard =
             await cardRepository.find(card.evolutionCardId);
         if (evoCard.spreadImageUrl != null) {
-          images.add(new CardSpreadImage(evoCard.name, evoCard.spreadImageUrl));
+          images.add(new Container(
+            child: new CardSpreadImage(evoCard.name, evoCard.spreadImageUrl),
+            padding: const EdgeInsets.only(top: 16.0),
+          ));
         }
       }
     });
 
+    if (images.isEmpty) {
+      images.add(new Container(
+        child: new Center(
+          child: new Text(
+            'カードなし',
+            style: new TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        padding: const EdgeInsets.only(top: 16.0),
+      ));
+    }
+
     setState(() {
       cardImages = images;
+      isLoading = false;
     });
   }
 }
@@ -129,19 +128,21 @@ class CardSpreadImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new Container(
-          height: 32.0,
-          child: new Center(
-            child: new Text(name),
+    return new InkWell(
+      child: new CachedNetworkImage(
+        imageUrl: imageUrl,
+        placeholder: new Center(
+          child: new AspectRatio(
+            aspectRatio: 1.553,
+            child: new Container(),
           ),
         ),
-        new CachedNetworkImage(
-          imageUrl: imageUrl,
-          placeholder: new CircularProgressIndicator(),
-        )
-      ],
+      ),
+      onTap: () {
+        Scaffold
+            .of(context)
+            .showSnackBar(new SnackBar(content: new Text(name)));
+      },
     );
   }
 }
