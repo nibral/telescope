17324ce63c746f55565e320a425203aa6167b1f5
nibral <nibral@users.noreply.github.com>
2018-03-16 12:01:49 +0900

    add character search

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -7,6 +7,7 @@ import 'package:telescope/repository/character_repository.dart';
 import 'package:telescope/repository/repository_factory.dart';
 import 'package:telescope/ui/character_detail_page.dart';
 import 'package:telescope/ui/widget/local_cached_network_image.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -25,14 +26,60 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static final double _iconSize = 128.0;
 
+  SearchBar _searchBar;
+  String _searchQuery = '';
+  bool _isShowSearchBar = false;
+
   List<CharacterListItem> _characterList = [];
   Map<int, Widget> _icons = new Map();
+
+  _MyHomePageState() {
+    _searchBar = new SearchBar(
+      inBar: true,
+      setState: (fn) {
+        _searchBar.controller.clear();
+        _searchQuery = '';
+        _isShowSearchBar = !_searchBar.isSearching;
+        setState(fn);
+      },
+      onSubmitted: (query) {
+        setState(() {
+          _searchQuery = query;
+        });
+      },
+      showClearButton: false,
+      clearOnSubmit: false,
+      closeOnSubmit: false,
+      buildDefaultAppBar: (BuildContext context) {
+        return new AppBar(
+          title: new Text('telescope'),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          elevation: 4.0,
+          actions: <Widget>[
+            _searchBar.getSearchAction(context),
+            new IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: _handleRefreshButtonPressed,
+            ),
+          ],
+        );
+      },
+    );
+    _searchBar.controller?.addListener(_handleSearchQueryChanged);
+  }
 
   @override
   void initState() {
     super.initState();
 
     _loadCharacterList();
+  }
+
+  @override
+  void dispose() {
+    _searchBar.controller?.removeListener(_handleSearchQueryChanged);
+    super.dispose();
   }
 
   @override
@@ -45,20 +92,36 @@ class _MyHomePageState extends State<MyHomePage> {
       SystemChrome.setEnabledSystemUIOverlays([]);
     }
 
+    if (_isShowSearchBar) {
+      return new Scaffold(
+        appBar: _searchBar.build(context),
+        body: _searchQuery.isEmpty
+            ? new Center()
+            : new ListView(
+                children: _characterList.where((c) {
+                  return c.name.contains(_searchQuery) ||
+                      c.nameKana.contains(_searchQuery);
+                }).map((e) {
+                  return new InkWell(
+                    onTap: () => _showCharacterDetail(context, e),
+                    child: new ListTile(
+                      key: new Key(e.id.toString()),
+                      title: new Text(e.name),
+                      subtitle: new Text(e.nameKana),
+                      trailing: new SizedBox(
+                        child: _icons[e.id],
+                        height: 48.0,
+                        width: 48.0,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+      );
+    }
+
     return new Scaffold(
-      appBar: isPortrait
-          ? new AppBar(
-              title: new Text('telescope'),
-              backgroundColor: Colors.black,
-              centerTitle: true,
-              actions: <Widget>[
-                new IconButton(
-                  icon: const Icon(Icons.refresh),
-                  onPressed: _handleRefreshButtonPressed,
-                ),
-              ],
-            )
-          : null,
+      appBar: isPortrait ? _searchBar.build(context) : null,
       body: _characterList.isEmpty
           ? new Center(
               child: new CircularProgressIndicator(
@@ -76,14 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 itemBuilder: (BuildContext context, int index) {
                   CharacterListItem character = _characterList[index];
                   return new InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(new MaterialPageRoute(
-                            builder: (_) => new CharacterDetailPage(
-                                character.id,
-                                character.name,
-                                character.cardIdList),
-                          ));
-                    },
+                    onTap: () => _showCharacterDetail(context, character),
                     child: new Stack(
                       children: <Widget>[
                         _icons[character.id],
@@ -157,6 +213,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _showCharacterDetail(BuildContext context, CharacterListItem character) {
+    Navigator.of(context).push(new MaterialPageRoute(
+          builder: (_) => new CharacterDetailPage(
+              character.id, character.name, character.cardIdList),
+        ));
+  }
+
   void _handleRefreshButtonPressed() {
     setState(() {
       _characterList = [];
@@ -164,5 +227,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     (new CacheManager()).clear();
     _loadCharacterList(refresh: true);
+  }
+
+  void _handleSearchQueryChanged() {
+    setState(() {
+      _searchQuery = _searchBar.controller.value.text;
+    });
   }
 }
